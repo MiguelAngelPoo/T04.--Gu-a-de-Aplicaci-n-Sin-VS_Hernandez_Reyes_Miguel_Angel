7fa772ecc01f6ce1e5b68634c9f8646b4d8492b3
MiguelAngelPoo <55799100+MiguelAngelPoo@users.noreply.github.com>
2019-11-27 22:00:40 -0800

    Add files via upload

---
+++ Practicas_Poo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicas_Poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             //instanciamos y mandamos a llamar
+            Principal PR = new Principal();
+            PR.capturardatos();
+        }
+    }
+}
---
+++ Practicas_Poo/Valores.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicas_Poo
+{
+    class Valores
+    {
+        //encapsulamos los datos 1,2 y 3 para ser usados despues
+        public double Dato1 { get; set; }
+
+        public double Dato2 { get; set; }
+
+        public double Dato3 { get; set; }
+    }
+}
